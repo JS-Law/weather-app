7aa58885d82162d6ed46961f80d830d80e06e08d
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-09-17 18:03:50 -0400

    Add style import, fixing font load issue

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import './styles.css'
 import 'material-icons/iconfont/material-icons.css';
+
+
+const key = 'T733FBTX9Q7J4EG5DY5Y7LLSU'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    mode: "development",
+    entry: "./src/index.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
             },
             {
-                test: /\.('woff|woff2|eot|ttf|otf')$/i,
-                type: 'asset/resource',
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            inject: 'body',
-            template: './src/index.html',
+            inject: "body",
+            template: "./src/index.html",
+            meta: {
+                preconnect: {
+                    rel: "preconnect",
+                    href: "https://fonts.googleapis.com",
+                    crossorigin: true,
+                },
+                preconnect2: {
+                    rel: "preconnect",
+                    href: "https://fonts.gstatic.com",
+                    crossorigin: true,
+                },
+            },
         }),
-    ]
+    ],
 };
